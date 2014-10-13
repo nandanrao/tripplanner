200086747e8cee60c49f1a9d0e0c418d3e7eaa58
Nandan Rao <nandanmarkrao@gmail.com>
2014-10-13 12:10:44 -0400

    worked on otherResults

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
-var test = Handlebars.templates.trip({tripname: 'booyah'});
+var test = Handlebars.templates.trip();
 document.body.innerHTML = test;
-
+  
 
 
 var gmaps = (function(){
@@ -18,11 +18,12 @@ var gmaps = (function(){
     });
   });
 
-  var lookup = function(name, resolve){
+  var lookup = function(place, callback){
+    // console.log('gmaps lookup');
     // Check if we have users location, if we don't, get it!
     if (!current_location){
       store_location.then(function(){
-        lookup(name, resolve)
+        lookup(place, callback)
       })
     }
     // If we have users location, lookup the item!
@@ -30,17 +31,22 @@ var gmaps = (function(){
       var request = {
         location: current_location,
         radius: '20000',
-        keyword: name,
+        keyword: place.name,
       }
       googleSearch.nearbySearch(request, function(results, status){
-        // create place and return it!
-        var place = {
-          id: results[0].id,
-          name : results[0].name,
-          address : results[0].vicinity,
-          LatLng : results[0].geometry.location,
+        // add properties to place and return it!
+        place.g_id = results[0].id;
+        place.init_str = place.name;
+        place.name = results[0].name;
+        place.address = results[0].vicinity;
+        place.LatLng = results[0].geometry.location;
+        place.otherResults = [];
+
+        for (var i=1, len=results.length; i<len; i++){
+          place.otherResults.push(results[i]);
         }
-        resolve(place)
+
+        callback(place)
       })
     }
   };
@@ -172,8 +178,8 @@ var routeCalculator = (function(){
     var totalDuration = 0;
     for (var i=0, len=arr.length; i<len-1; i++){
       hash.forEach(function(el){
-        if (arr[i].id === el.start.id){
-          if (arr[i+1].id === el.end.id){
+        if (arr[i].g_id === el.start.g_id){
+          if (arr[i+1].g_id === el.end.g_id){
             totalDuration += el.duration
           }
         }
@@ -202,27 +208,173 @@ var routeCalculator = (function(){
 
 
 
-var lincoln = new Promise(function(resolve, reject){
-  gmaps.lookup('lincoln center', resolve)
-});
+// Handles submitting of places for bestRoute calculations, and return display... 
+var controller = (function(){
+  // initialize array for all the google lookups
+  var foundArray = []
+  // Subscribe gmaps.lookup method, add promise to array
+  PubSub.subscribe('newInput', function(msg, obj){
+    var found = new Promise(function(resolve, reject){
+        gmaps.lookup(obj, resolve)
+    });
+    foundArray.push(found)
+  });
 
-var coop = new Promise(function(resolve, reject){
-  gmaps.lookup('parkslope food coop', resolve)
-});
+  function enterKey(e){
+    e.target.style.display = 'none';
+    document.getElementById('inputForm').style.display = 'none';
+    // check to see if all locations have been found, then calculate best route.
+    Promise.all(foundArray).then(function(results){
+      routeCalculator.calculate(storage.places, function(route){
+        // Change the 'pos' property on each place object, based on best route
+        for (var i=0, len=route.places.length; i<len; i++){
+          route.places[i].pos = i+1;
+        }
+        // create a duration element and add it (should be template?)
+        var dur = document.createElement('li');
+        document.getElementById('places').appendChild(dur);
+        dur.style.order = route.places.length + 1;
+        dur.innerHTML = Math.round(route.duration) + ' min'
+      })
+    })
+  }
+  return {
+    enter: enterKey,
+  }
+})();
 
-var fullstack = new Promise(function(resolve, reject){
-  gmaps.lookup('fullstack academy of code', resolve)
-});
 
-var videofree = new Promise(function(resolve, reject){
-  gmaps.lookup('video free brooklyn', resolve)
-});
+// Creates new place objects from form input.
+var input = (function(){
+  // if store is empty, set counter to 0, else... 
+  var counter = 0;
+  var createPlaceObj = function(str){
+    var place = {
+      _id: '_' + Math.random().toString(36).substr(2, 9),
+      name: str,
+      pos : ++counter,
+    };
+    PubSub.publish('newInput', place) 
+  };
+  var setCounter = function(num){
+    counter = num
+  };
+  return {
+    newInput: createPlaceObj,
+  }
+})();
 
 
-// TODO: make sure results come back in order? 
-Promise.all([lincoln, coop, fullstack, videofree]).then(function(arr){
-  routeCalculator.calculate(arr, function(results){
-    console.log(results)
+
+// Add onsubmit listener to the input!
+(function(){
+var form = document.getElementById('inputForm')
+form.onsubmit = function(e){
+  var textBox = e.target.elements['input']; 
+  input.newInput(textBox.value);
+  textBox.value = '';
+  return false;
+}
+})()
+
+
+
+// View Changes on new input
+PubSub.subscribe('newInput', function(msg, place){
+  // Create the DOM element for this place!
+  var li = Handlebars.templates.place(place);
+  var ul = document.createElement('ul');
+  ul.innerHTML = li;
+  el = ul.firstChild;
+
+  // object to use mucho
+  li = {
+    el : el,
+    name: el.getElementsByClassName('name')[0],
+    address: el.getElementsByClassName('address')[0],
+    alternate: false,
+  };
+  
+  var ul = document.createElement('ul')
+  ul.classList.add('alternates');
+
+  li.name.onclick = function(e){
+    if (!li.alternate){
+      ul.style.display = 'block';
+      li.alternate = true;
+      ul.innerHTML = Handlebars.templates.alternates(place);
+      li.el.appendChild(ul);
+
+      console.log(ul.children);
+      for (var i=0, len=ul.children.length; i<len; i++){
+        ul.children[i].onclick = function(e){
+          var g_id = this.getAttribute('data-google-id');
+          place.otherResults.forEach(function(el){
+            if (el.id === g_id){
+              console.log(el);
+              place.g_id = el.id;
+              place.name = el.name;
+              place.address = el.vicinity;
+              place.LatLng = el.geometry.location;
+            }
+          })
+          this.parentNode.innerHTML = '';
+          li.alternate = false;
+        }
+      }
+    }
+    else if (li.alternate){
+      li.alternate = false;
+      ul.innerHTML = '';
+    }
+  }
+
+  document.getElementById('places').appendChild(li.el);
+
+  // Put a listener on each place object to bind view to object properties
+  Object.observe(place, function(changes){
+    changes.forEach(function(change){
+      if (change.name === 'name'){
+        li.name.innerHTML = change.object.name;
+      } 
+      if (change.name === 'address'){
+        li.address.innerHTML = change.object.address;
+      }
+      if (change.name === 'pos'){
+        li.el.style.order = change.object.pos;
+      }
+    })
   })
-});
 
+  // Show CALCULATE button when at least 3 places are entered.  
+  if (places.children.length > 2){
+    var calculateBtn = document.getElementById('calculate')
+    console.log(calculateBtn);
+    calculateBtn.style.display = 'inline-block'; 
+    calculateBtn.onclick = controller.enter;
+  }
+
+});
+ 
+
+// Stores all our place objects in a nice array!
+var storage = (function(){
+  var places = [];
+  var tripInfo = {};
+
+  function set(place){
+    places.push(place)
+  }
+
+  function findById(id){
+
+  }
+
+  PubSub.subscribe('newInput', function(msg, obj){
+    set(obj);
+  });
+
+  return {
+    places: places,
+  }
+})();
--- public/javascripts/templates.js
+++ public/javascripts/templates.js
@@ -1,9 +1,31 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['trip'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return "<p class=\"trip\">"
-    + escapeExpression(((helper = (helper = helpers.tripname || (depth0 != null ? depth0.tripname : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"tripname","hash":{},"data":data}) : helper)))
-    + "hel!!!</p>\n<div id=\"directions\"></div>";
+templates['alternates'] = template({"1":function(depth0,helpers,partials,data) {
+  var lambda=this.lambda, escapeExpression=this.escapeExpression;
+  return "<li class=\"alternate\" data-google-id=\""
+    + escapeExpression(lambda((depth0 != null ? depth0.id : depth0), depth0))
+    + "\"><span class=\"name\">"
+    + escapeExpression(lambda((depth0 != null ? depth0.name : depth0), depth0))
+    + "</span><span class=\"address\">"
+    + escapeExpression(lambda((depth0 != null ? depth0.vicinity : depth0), depth0))
+    + "</span></li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, buffer = "";
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.otherResults : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { buffer += stack1; }
+  return buffer;
 },"useData":true});
+templates['place'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "<li class=\"place\" data-place-id=\""
+    + escapeExpression(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"_id","hash":{},"data":data}) : helper)))
+    + "\" style=\"order: "
+    + escapeExpression(((helper = (helper = helpers.pos || (depth0 != null ? depth0.pos : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"pos","hash":{},"data":data}) : helper)))
+    + "\"><span class=\"name\">"
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "</span> <span class=\"address\"></span></li>";
+},"useData":true});
+templates['trip'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<div id=\"directions\"></div>\n<div class=\"main\">\n  <ul class=\"places\" id=\"places\"></ul>\n  <form action=\"\" id=\"inputForm\">\n  <input type=\"text\" name=\"input\" id=\"input\" autofocus>\n  </form>\n  <button id=\"calculate\">Calculate</button>\n</div>\n\n";
+  },"useData":true});
 })();
